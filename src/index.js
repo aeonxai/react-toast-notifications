fdf60f1cc248730135e20c603d1e51678d80458a
Marzuk Kamal <marzuk9999@gmail.com>
2022-07-11 15:56:14 +0200

    update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export { ToastContainer as DefaultToastContainer } from './ToastContainer';
-export { DefaultToast } from './ToastElement';
-export {
-  ToastConsumer,
-  ToastProvider,
-  withToastManager,
-  useToasts,
-} from './ToastProvider';
